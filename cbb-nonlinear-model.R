52e60e9cc0b882ce3e49347dc30909f0733cc663
johnwoodill <johnwoodill@gmail.com>
2016-05-25 17:43:45 -1000

    asdf

--- cbb-nonlinear-model.R
+++ cbb-nonlinear-model.R
@@ -22,6 +22,9 @@ price <- 2
 max_inf <- .25
 harvest <- c(0, 0, 0, 0, 0, 0, 0, 0, .32, .48, .12, .07)
 
+# All possible combinations
+zeroone <- expand.grid(0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1)
+
 # Markov Transition Matrices
 
 # No Spray
@@ -42,6 +45,24 @@ sp <- matrix(c(0.9 , 0.1 , 0   , 0,
 ii <- c(0.90,0.10,0,0)
 mat <- matrix(nrow=12, ncol = 4)
 
+
+# Data frame for storing variables
+dat <- data.frame(month = 1:12,
+                  spraystrat = rep(0,12),
+                  inflevel = rep(0,12),
+                  harvest = rep(0,12),
+                  cherry_harvest = rep(0,12),
+                  damage = rep(0,12),
+                  cherry_ending = rep(0,12),
+                  revenue = rep(0,12),
+                  spray_cost = rep(0,12),
+                  harvest_cost = rep(0,12),
+                  net_revenue = rep(0,12))
+
+#-------------------
+# Fuctions
+#-------------------
+
 # Markov Process Function
 markov <- function(spraystrat){
   for (i in 1:12){
@@ -59,29 +80,9 @@ mat$month <- c(1:12)
 return(mat)
 }
 
-  
 #------------------------
 # Model Optimization
 #------------------------
-
-# All possible combinations
-zeroone <- expand.grid(0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1,0:1)
-
-# Data frame for storing variables
-dat <- data.frame(month = rep(0,12),
-                  spraystrat = rep(0,12),
-                  inflevel = rep(0,12),
-                  harvest = rep(0,12),
-                  cherry_harvest = rep(0,12),
-                  damage = rep(0,12),
-                  cherry_ending = rep(0,12),
-                  revenue = rep(0,12),
-                  spray_cost = rep(0,12),
-                  harvest_cost = rep(0,12),
-                  net_revenue = rep(0,12))
-
-dat$month <- 1:12
-check <- data.frame()
 
 # Function to build optimization scenario
 optimal <- function(x){
@@ -89,7 +90,6 @@ optimal <- function(x){
   newdat$spraystrat <- t(x)
   markov_matrix <- markov(x)
   newdat$inflevel <- markov_matrix$cd
-  #newdat$inflevel <- c(.09,.09,.09,.09,.09,.1026,.117,.1333,.152,.1733,.2339,.3158)
   newdat$harvest <- harvest
   newdat$cherry_harvest = acres*proj_cherry*newdat$harvest
   newdat$damage = newdat$cherry_harvest*newdat$inflevel
@@ -114,23 +114,27 @@ optimal <- function(x){
   }
 
 # Iterate through each combination
-var <- 0   
-pb <- txtProgressBar(min = 0, max = nrow(zeroone), initial = 0)
-stepi <- 0
-
-for (i in 1:nrow(zeroone)){
-  x <- zeroone[i,]
-  newmat <- optimal(x)
-  x$net_revenue <- sum(newmat$net_revenue)
-  if(x$net_revenue > var){
-    var <- x$net_revenue
-    maxmat <- x
+comb <- function(var = 0, zeroone = zeroone){
+  pb <- txtProgressBar(min = 0, max = nrow(zeroone), initial = 0)
+  stepi <- 0
+  
+  for (i in 1:nrow(zeroone)){
+    x <- zeroone[i,]
+    newmat <- optimal(x)
+    x$net_revenue <- sum(newmat$net_revenue)
+    if(x$net_revenue > var){
+      var <- x$net_revenue
+      maxmat <- x
+    }
+    stepi = stepi + 1
+    setTxtProgressBar(pb, stepi)
   }
-  stepi = stepi + 1
-  setTxtProgressBar(pb, stepi)
-}
+  # Find maximum value
+  optimal_scenario <- optimal(maxmat[1:12])
+  optimal_scenario
+  markov(maxmat[1:12])
+  return(optimal_scenario)
+  }
 
-# Find maximum value
-optimal_scenario <- optimal(maxmat[1:12])
-optimal_scenario
-markov(maxmat[1:12])
+comb(0, zeroone)
+comb()
